b8ac27275e3fb7bd5894d901150ef0d4bd037687
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-13 15:43:46 -0600

    added table associations and updated server.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product')
+const Category = require('./Category')
+const Tag = require('./Tag')
+const ProductTag = require('./ProductTag')
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Foreign key in Product
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // Foreign key in Product
+  onDelete: 'CASCADE', // When a Category is deleted, its Products will be deleted
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, // Join table
+  foreignKey: 'product_id', // Foreign key in ProductTag
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, // Join table
+  foreignKey: 'tag_id', // Foreign key in ProductTag
+})
 
+// Export the models
 module.exports = {
   Product,
   Category,
   Tag,
   ProductTag,
-};
+}
